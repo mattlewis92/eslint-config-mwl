248c9494c852abeadaab4c9bd64ba3652ba20fca
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2015-07-05 14:29:19 +0100

    Initial commit with my config

---
+++ test.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "rules": {
+    "no-unused-expressions": 0
+  },
+  "env": {
+    "mocha": true
+  },
+  "globals": {
+    "expect": true,
+    "assert": true,
+    "sinon": true,
+    "inject": true
+  }
+};
